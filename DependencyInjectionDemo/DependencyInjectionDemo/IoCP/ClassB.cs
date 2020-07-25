1993f02d2019e4dbcdfdcfad86004b4b5d8f78a4
Carlos <moonprince1@hotmail.com>
2020-07-25 19:03:16 -0400

    Add IoC example

---
+++ DependencyInjectionDemo/DependencyInjectionDemo/IoCP/ClassB.cs
@@ -0,0 +1,24 @@
+﻿namespace DependencyInjectionDemo.IoCP
+{
+    public class ClassB
+    {
+        private ClassA classA;
+
+        public ClassB()
+        {
+            this.classA = new ClassA();
+        }
+    }
+
+    #region IoCP_ClassB
+    public class IoCP_ClassB
+    {
+        private ClassA classA;
+
+        public IoCP_ClassB()
+        {
+            this.classA = FactoryClass.CreateClassA();
+        }
+    }
+    #endregion
+}
---
+++ DependencyInjectionDemo/UnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
