491df4c366c474f7e4f68525b742621eef091da5
Carlos <moonprince1@hotmail.com>
2020-07-26 15:02:46 -0400

    Add recipient validation

--- DependencyInjectionDemo/NinjectDependencyInjectionDemo/MessageSender/SMSMessageSender.cs
+++ DependencyInjectionDemo/NinjectDependencyInjectionDemo/MessageSender/SMSMessageSender.cs
@@ -6,6 +6,11 @@ namespace NinjectDependencyInjectionDemo.MessageSender
     {
         public void Send(string message, string recipient)
         {
+            if (string.IsNullOrWhiteSpace(recipient))
+            {
+                throw new ArgumentException("Cannot be null or empty string", nameof(recipient));
+            }
+
             Console.WriteLine("SMS recipient={0} : data={1}", recipient, message);
         }
     }
